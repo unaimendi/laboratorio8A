50f8d3b521153c55267e9661a3c6b5cbfc4a702d
Unai <umendizabal@gmail.com>
2024-03-29 12:33:15 +0100

    ejercicio terminado

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
-import "./style.css";
+import { obtenPacientesAsignadosAPediatria, obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios, activarProtocoloUrgencia, reasignaPacientesAMedicoFamilia, HayPacientesDePediatria, cuentaPacientesPorEspecialidad } from "./motor";
+import { pacientes } from "./modelo";
 
-console.log("Hello Typescript!");
+console.log(obtenPacientesAsignadosAPediatria(pacientes));
+console.log(obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes));
+console.log(activarProtocoloUrgencia(pacientes));
+console.log(reasignaPacientesAMedicoFamilia(pacientes));
+console.log(HayPacientesDePediatria(pacientes));
+console.log(cuentaPacientesPorEspecialidad(pacientes));
